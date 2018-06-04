feb1bb6e8e0a0f248c1024767e4bf61248e8adeb
Kinto D <kiswanto.d21@gmail.com>
2018-06-04 09:21:55 +0700

    change config

--- config.py
+++ config.py
@@ -12,7 +12,7 @@
 # amazon books
 DATASET_DIR = '/home/g40/PycharmProjects/finaltask-project/dataset/amazon-office/ratings_Books_Product-2018-03-11 00:25:56.789384.min'
 DATASET_DELIMITER = ','
-MODEL_LOCATION = 'save_data/amazon-books-f50-e06/real_final/reg'
+MODEL_LOCATION = 'saved_model/amazon_books_f50_e06_i30/reg'
 SHORT_HEAD_BOUND = 85
 
 
